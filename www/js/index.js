288af39e9029cc905834f9f0f30d4d452b388d90
mister-no-one <de_prada.gr@me.com>
2020-03-11 17:17:16 +0100

    addPoints function in progress

--- www/js/index.js
+++ www/js/index.js
@@ -1,43 +1,68 @@
 var score = 0;
 
-document.querySelector(".addOne").addEventListener('click',function(){
-    console.log('+1 point');
-    score++;
+function addPoints(score){
+    document.querySelector(".addOne").addEventListener('click',function(){
+        console.log('+1 point');
+        score++;
+        db.transaction(function(tx) {
+            tx.executeSql('INSERT INTO MatchLog (dateTime, score, teamid) VALUES(?,?,?)', ['2020-09-21', 1 , 1 ]);
+        }
+    });
+
+// FUNCTION POUR INSERTION EN BASE
+// JUSTE REQUETE  tx.executeSql('INSERT INTO MatchLog (dateTime, score, teamid) VALUES(?,?,?)', ['2020-09-21', 1 , 1 ]);
+
+
+
+
+
+
+
+    document.querySelector(".addTwo").addEventListener('click',function(){
+        console.log('+3 points');
+        score += 3;
+    });
+
     document.querySelector(".score").innerHTML = score;
-});
+};
 
-document.querySelector(".addTwo").addEventListener('click',function(){
-    console.log('+3 points');
-    score += 3;
-    document.querySelector(".score").innerHTML = score;
-});
+addPoints(score);
+console.log(score);
 
+// document.querySelector(".addOne").addEventListener('click',function(){
+//     console.log('+1 point');
+//     score++;
+//     document.querySelector(".score").innerHTML = score;
+// });
 
+// document.querySelector(".addTwo").addEventListener('click',function(){
+//     console.log('+3 points');
+//     score += 3;
+//     document.querySelector(".score").innerHTML = score;
+// });
 
 // Base
 var db = null;
 
 document.addEventListener('deviceready',function(event){
 
- if (window.cordova.platformId === 'browser') {
-    db = window.openDatabase('MatchLog', '1.0', 'Data', 2*1024*1024);
+   if (window.cordova.platformId === 'browser') {
+    db = window.openDatabase('ScoreGames', '1.0', 'Data', 2*1024*1024);
     console.log("Opening browser database"); } 
     else {
-        db = window.sqlitePlugin.openDatabase({name: 'MatchLog.db', location: 'default'});
+        db = window.sqlitePlugin.openDatabase({name: 'ScoreGames.db', location: 'default'});
         console.log("Opening mobile (plugin) database");
     }
 
     // db ...
     db.transaction(function(tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS MatchLog (dateTime, score, teamid)');
-        tx.executeSql('INSERT INTO MatchLog VALUES (?,?,?,?)', [1,123, 101, 1]);
+        tx.executeSql('CREATE TABLE IF NOT EXISTS MatchLog (id INTEGER PRIMARY KEY, dateTime, score, teamid)');
+        tx.executeSql('INSERT INTO MatchLog (dateTime, score, teamid) VALUES("2020-03-10", '+ score +', 1)');
     }, function(error) {
         console.log('Transaction ERROR: ' + error.message);
     }, function() {
         console.log('Populated database OK');
     });
-
-
 });
 
 
